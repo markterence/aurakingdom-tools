5ecea164f6ddacff4bd20d1f50e9e9ac92c41e01
markterence <markterencetiglao@yahoo.com>
2017-10-28 17:09:07 +0800

    Update install-samba.sh

--- setup/install-samba.sh
+++ setup/install-samba.sh
@@ -1,30 +1,4 @@
 #!/bin/sh
 sudo apt-get update
 apt-get install -y samba samba-common python-glade2 system-config-samba
-sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
-sudo nano /etc/samba/smb.conf
 
-cat > /etc/samba/smb.conf <<EOF
-[global]workgroup = WORKGROUP
-server string = Samba Server %v
-netbios name = ubuntu
-security = user
-map to guest = bad user
-dns proxy = no
-
-
-[secured]
-path = /genshinserver
-valid users = @smbgrp
-guest ok = no
-writable = yes
-browsable = yes
-EOF
-
-addgroup smbgrp
-adduser genshin -G smbgrp
-smbpasswd -a genshin
-
-mkdir -p genshinserver
-
-service smbd restart
