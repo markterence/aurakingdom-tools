63303c1b6496976d19d1b8b265f6199bb4923874
markterence <markterencetiglao@yahoo.com>
2017-10-28 17:11:57 +0800

    Create samba-config.sh

---
+++ setup/samba-config.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
+cat > /etc/samba/smb.conf <<EOF
+[global]workgroup = WORKGROUP
+server string = Samba Server %v
+netbios name = ubuntu
+security = user
+map to guest = bad user
+dns proxy = no
+
+
+[secured]
+path = /genshinserver
+valid users = @smbgrp
+guest ok = no
+writable = yes
+browsable = yes
+EOF
+addgroup smbgrp
+adduser genshin -G smbgrp
+smbpasswd -a genshin
+mkdir -p genshinserver
+service smbd restart
