4614e483bcd0ee28fd66d0c99967d2b88f622b89
Mark Terence <markterencetiglao@gmail.com>
2017-11-03 20:14:11 +0800

    added create db for psql

---
+++ sql/create-db.sql
@@ -0,0 +1,4 @@
+create database ffaccount encoding 'UTF8' template template0;
+create database ffdb1 encoding 'UTF8' template template0;
+create database ffmember encoding 'UTF8' template template0;
+create database itemmall encoding 'UTF8' template template0;
